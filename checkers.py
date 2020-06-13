379f3f9d9a8b105f3b7adfb70eb4367d64aa4fbc
Jimmy <jimmyjames2236@gmail.com>
2020-06-13 10:10:44 -0500

    asthetic code change.

--- checkers.py
+++ checkers.py
@@ -73,6 +73,7 @@ def make_squares():
             continue
         color_counter += 1  # make next square a different color
 
+
 def set_initial_piece_position():
     brown_pieces = [1,3,5,7,8,12,10,14,17,19,21,23]
     red_pieces = [40,42,44,46,49,51,53,55,56,58,60,62]
@@ -82,7 +83,6 @@ def set_initial_piece_position():
 
     for piece in red_pieces:
         piece_list[piece] = 2
-
 
 
 def attach_pieces():
